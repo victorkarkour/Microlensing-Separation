52ff22afa9209d3f69d2793761548fb771d5df3c
victorkarkour <victorkarkour@outlook.com>
2025-05-16 10:46:26 -0400

    commit

--- Sep.py
+++ Sep.py
@@ -1,19 +1,38 @@
 import astropy.constants as ac
 import numpy as np
-import scipy.differentiate as derivative
+import scipy.differentiate as diff
+import scipy.optimize as sc
 
-def NewtRaf(fE, n = 2):
-    initial = fE
-    Elist = []
-    for i in range(n-1):
-        if i == 0:
-            Eprime = derivative(fE,Elist[i])
-            Elist.append(initial-(fE/Eprime))
-        Eprime = derivative(fE,Elist[i])
-        Elist.append(Elist[i]-(fE/Eprime))
-    return(Elist[-1])
+def NewtRaf(M, e):
+    initial = M+e*np.sin(M) # Bascially starting with M as our initial guess
+    Solution = np.zeros_like(M)
+    
+    for i in range(len(M)):
+        Ei = initial[i]
+        Mi = M[i]
+        
+        Solution[i] = sc.newton(Kepler, Ei, fprime = DKepler, args = (Mi,e))
+    
+    # Previous attempt at solving for E
+    # Elist = []
+    # for i in range(n-1):
+    #     if i == 0:
+    #         Eprime = diff.derivative(fE,Elist[i])
+    #         Elist.append(initial-(fE/Eprime))
+    #     Eprime = diff.derivative(fE,Elist[i])
+    #     Elist.append(Elist[i]-(fE/Eprime))
+    # return(Elist[-1])
+    
+    return(Solution)
 
-def OrbGeo(t, a=1, w = 0, W = 0, i = 0, e = 0, n=2):
+def Kepler(En,Mn,ec):
+    print(En)
+    return(En-ec*np.sin(En)-Mn)
+
+def DKepler(En,Mn,ec):
+    return(1-ec*np.cos(En))
+
+def OrbGeo(t, a=1, w = 0, W = 0, i = 0, e = 0):
     """
     Creates and calculates the X and Y axis of a planet's orbit
     """
@@ -31,10 +50,21 @@ def OrbGeo(t, a=1, w = 0, W = 0, i = 0, e = 0, n=2):
     # Equation for E (G*M_sun = 1 in Solar Units)
     P = np.sqrt((4*np.pi)/(1)*a**3)
     # Function for E
-    fE = (2*np.pi/P) * (t-t0) + e*np.sin(E) - E
-    E = NewtRaf(fE,n)
+    M = (2*np.pi/P) * (t-t0)
+    Et = NewtRaf(M, e)
     
-    Xt = np.cos(E) - e
-    Yt = np.sqrt(1-e**2)*np.sin(E)
+    Xt = np.cos(Et) - e
+    Yt = np.sqrt(1-e**2)*np.sin(Et)
     
+    xt = A*Xt + F*Yt
+    yt = B*Xt + G*Yt
     
+    return(xt, yt)
+def pltOrb(t, a=1, w = 0, W = 0, i = 0, e = 0):
+    return
+    
+
+
+
+t = np.linspace(0,3,4)
+print(OrbGeo(t,e=0.1))
